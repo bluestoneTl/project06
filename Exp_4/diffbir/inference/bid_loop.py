0aed62a01cdd6fa1e1be843d659df663a91b1b1c
bluestone <2055325983@qq.com>
2025-02-20 17:21:33 +0800

    111

---
+++ Exp_4/diffbir/inference/bid_loop.py
@@ -0,0 +1,55 @@
+import numpy as np
+from PIL import Image
+from omegaconf import OmegaConf
+
+from .loop import InferenceLoop, MODELS
+from ..utils.common import (
+    instantiate_from_config,
+    load_model_from_url,
+    trace_vram_usage,
+)
+from ..pipeline import (
+    SwinIRPipeline,
+    SCUNetPipeline,
+)
+from ..model import SwinIR, SCUNet
+
+
+class BIDInferenceLoop(InferenceLoop):
+
+    def load_cleaner(self) -> None:
+        if self.args.version == "v1":
+            config = "configs/inference/swinir.yaml"
+            weight = MODELS["swinir_general"]
+        elif self.args.version == "v2":
+            # config = "configs/inference/scunet.yaml"
+            # weight = MODELS["scunet_psnr"]                   # ！注意这里是尝试用v2去推理才改的
+            config = "configs/inference/swinir.yaml"
+            weight = MODELS["swinir_general"]
+        else:
+            config = "configs/inference/swinir.yaml"
+            weight = MODELS["swinir_realesrgan"]
+        self.cleaner: SCUNet | SwinIR = instantiate_from_config(OmegaConf.load(config))
+        model_weight = load_model_from_url(weight)
+        self.cleaner.load_state_dict(model_weight, strict=True)
+        self.cleaner.eval().to(self.args.device)
+
+    def load_pipeline(self) -> None:
+        # if self.args.version == "v1" or self.args.version == "v2.1":            # ！注意这里是尝试用v2去推理才改的
+        if self.args.version == "v1" or self.args.version == "v2.1" or self.args.version == "v2": 
+            pipeline_class = SwinIRPipeline
+        else:
+            pipeline_class = SCUNetPipeline
+        self.pipeline = pipeline_class(
+            self.cleaner,
+            self.cldm,
+            self.diffusion,
+            self.cond_fn,
+            self.args.device,
+        )
+
+    def after_load_lq(self, lq: Image.Image) -> np.ndarray:
+        lq = lq.resize(
+            tuple(int(x * self.args.upscale) for x in lq.size), Image.BICUBIC
+        )
+        return super().after_load_lq(lq)
